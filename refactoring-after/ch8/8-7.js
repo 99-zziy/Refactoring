307661addf6613785770191c73dc3a371b4b4edc
99-zziy <wldud8229@gmail.com>
2023-03-06 00:24:56 +0900

    반복문 쪼개기

---
+++ refactoring-after/ch8/8-7.js
@@ -0,0 +1,11 @@
+export function reportYoungestAgeAndTotalSalary(people) {
+  return `youngestAge: ${youngestAge(people)}, totalSalary: ${totalSalary(people)}`;
+}
+
+function youngestAge(people) {
+  return Math.min(...people.map((p) => p.age));
+}
+
+function totalSalary(people) {
+  return people.reduce((total,p) => (total += p.salary), 0)
+}
