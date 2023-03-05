6e2d052e98ece360ee49344e7523efdc60a094a3
99-zziy <wldud8229@gmail.com>
2023-03-05 12:45:26 +0900

    레코드 캡슐화하기

---
+++ refactoring-after/ch7/7-1.js
@@ -0,0 +1,22 @@
+class Organization {
+    #name;
+    #country;
+    constructor(name,country){
+        this.#name = name;
+        this.#country = country;
+    }
+
+    get name(){
+        return this.#name;
+    }
+    
+    get country(){
+        return this.#country;
+    }
+
+}
+const organization = new Organization('Acme Gooseberries','GB');
+
+organization.name = 'Dream Coding';
+console.log(organization.name);
+console.log(organization.country);
