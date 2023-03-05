82e1d0f9d70279565a284f6d69cbe319fad7b60f
99-zziy <wldud8229@gmail.com>
2023-03-05 23:03:28 +0900

    클래스 추출하기

---
+++ refactoring-after/ch7/7-5.js
@@ -0,0 +1,57 @@
+class Person {
+  #name;
+  #telephoneNumber;
+
+  constructor(name, areaCode, number) {
+    this.#name = name;
+    this.#telephoneNumber = new TelephoneNumber(areaCode, number)
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(arg) {
+    this.#name = arg;
+  }
+
+  get telephoneNumber() {
+    return this.#telephoneNumber.toString;
+  }
+
+}
+
+class TelephoneNumber {
+
+  #areaCode;
+  #number;
+  constructor(areaCode,number){
+    this.#areaCode = areaCode
+    this.#number = number
+  }
+
+  get areaCode() {
+    return this.#areaCode;
+  }
+
+  set areaCode(arg) {
+    this.#areaCode = arg;
+  }
+
+  get number() {
+    return this.#number;
+  }
+
+  set number(arg) {
+    this.#number = arg;
+  }
+
+  get toString(){
+    return `${this.#areaCode} ${this.#number}`
+  }
+
+}
+
+const person = new Person('엘리', '010', '12345678');
+console.log(person.name);
+console.log(person.telephoneNumber);
