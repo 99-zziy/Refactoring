0eeabc2869ad74851247a767cc6ed7435cc88825
99-zziy <wldud8229@gmail.com>
2023-03-06 18:51:00 +0900

    플래그 인수 제거하기

---
+++ refactoring-after/ch11/11-3.js
@@ -0,0 +1,18 @@
+// 예제 1
+function setWidth(value) {
+  this._height = value;
+}
+
+function setHeight(name) {
+  this._height = value;
+}
+
+// 예제 2
+class Concert {
+  regularBook(customer) {}
+  premiumBook(customer) {}
+}
+
+// 예제 3
+function switchOn();
+function switchOff();
