fce25a18f85a038ee9e40c3afceb52334d58a253
99-zziy <wldud8229@gmail.com>
2023-03-06 18:55:19 +0900

    객체 통째로 넘기기

---
+++ refactoring-after/ch11/11-4.js
@@ -0,0 +1,20 @@
+export function temperatureAlerts(room, plan) {
+  const alerts = [];
+  if (!plan.withinRange(room.daysTempRange)) {
+    alerts.push('room temperature went outside range');
+  }
+
+  return alerts;
+}
+
+export class HeatingPlan {
+  constructor(temperatureRange) {
+    this._temperatureRange = temperatureRange;
+  }
+
+  withinRange({bottom, top}) {
+    return (
+      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+    );
+  }
+}
